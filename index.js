7b8bd055313cd964ee3f1ecc64f4088a2ca39ed6
gajraj09 <gsr939988@gmail.com>
2026-08-05 09:57:49 +0530

    add database setup

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import app from './src/app.js'
+import connectDB from './src/db/connectDB.js';
+import {config} from "./src/config/config.js"
 
 
 
 
 
-const startServer = ()=>{
-    const port = process.env.PORT || 5000;
+const startServer = async ()=>{
+    await connectDB();
+    const port = config.port || 5000;
     app.listen(port , ()=>{
         console.log(`Server is running on http://localhost:${port}`);
     })
