f46c5bc5a583442ea5e29aa6ef2ec79d1dae62d5
gajraj09 <gsr939988@gmail.com>
2026-08-05 22:42:55 +0530

    create book and upload files in cloudinary and delete temp files

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,12 +1,52 @@
-import mongoose from "mongoose"
-import Book from "../models/book.model.js"
+import fs from "node:fs/promises";
+import mongoose from "mongoose";
+import Book from "../models/book.model.js";
+import createHttpError from "http-errors";
+import cloudinary from "../config/cloudinary_configs.js";
 
 
-const createBook = async(req,res,next)=>{
- console.log(req.file);
- req.json("/books hits")
+const createBook = async (req, res, next) => {
+  const files = req.files;
+  const coverImageFiles = files?.coverImage;
+  const bookFiles = files?.file;
 
+  if (!coverImageFiles?.length || !bookFiles?.length) {
+    return next(createHttpError(400, "coverImage and file are required."));
+  }
 
-}
+  const coverImageFile = coverImageFiles[0];
+  const bookFile = bookFiles[0];
 
-export {createBook}
+  try {
+    const uploadImage = await cloudinary.uploader.upload(coverImageFile.path, {
+      folder: "book-covers",
+      resource_type: "image",
+    });
+    const uploadFile = await cloudinary.uploader.upload(bookFile.path, {
+      folder: "book-files",
+      resource_type: "auto",
+    });
+
+    const newBook = await Book.create({
+      title: req.body.title || "Untitled",
+      author: "6a72e1e9d9e6b8bbe4920cda",
+      genre: req.body.genre || "Unknown",
+      coverImage: uploadImage.secure_url || uploadImage.url,
+      file: uploadFile.secure_url || uploadFile.url,
+    });
+
+    await fs.unlink(coverImageFile.path);
+    await fs.unlink(bookFile.path);
+    
+
+    return res.status(201).json(newBook);
+  } catch (error) {
+    await fs.unlink(coverImageFile.path);
+    await fs.unlink(bookFile.path);
+    return next(
+      createHttpError(500, error.message || "Failed to create book."),
+    );
+  }
+};
+
+export { createBook };
